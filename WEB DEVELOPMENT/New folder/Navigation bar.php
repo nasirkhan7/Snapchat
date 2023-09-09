bf869f92d44621a0cb4ee07a0090de7d6d3519bb
N A S I R K H A N <nasirkhana842@gmail.com>
2023-09-09 12:21:06 -0700

    first commit

---
+++ WEB DEVELOPMENT/New folder/Navigation bar.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Navigation Bar</title>
+	<link rel="stylesheet" href="https://fontawesome.com/">
+	<style>
+		.icon-bar{
+			width: 90px;
+			background-color: #555 ;
+		}
+		.icon-bar a{
+			display: block;
+			text-align: center;
+			padding: 15px;
+			transition: all 0.3 ease;
+			color: white;
+			font-size: 36px;
+		}
+		.active{
+			background-color: #04AA6D;
+		}
+		.icon-bar a:hover{
+			background-color: #000;
+		}
+	</style>
+</head>
+<body>
+<div class="icon-bar">
+  		<a class="active" href="#"><i class="fa fa-home"></i></a>   
+			<a href="#"><i class="fa fa-search"></i></a>  
+			<a href="#"><i class="fa fa-envelope"></i></a>  
+			<a href="#"><i class="fa fa-globe"></i></a> 
+			<a href="#"><i class="fa fa-trash"></i></a> 
+		</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
